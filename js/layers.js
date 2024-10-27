c9229997fda758e38b18f064764284c4c4453e56
fqiim <3281976013@qq.com>
2024-10-27 19:01:35 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -26,11 +26,31 @@ addLayer("p", {
     ],
     layerShown(){return true}
 })
-addLayer("o", {    upgrades: {
-
-    },        11: {    title: "Make this whatever you want!",
-    description: "Double your point gain.",
-    cost: new Decimal(1),
-
-        },
-	       if (hasUpgrade('p', 11)) gain = gain.times(2)
+addLayer("o", {
+    name: "original", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "O", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		oi: new Decimal(0),
+    }},
+    color: "#4BDC13",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "oi", // Name of prestige currency
+    baseResource: "oi", // Name of resource prestige is based on
+    baseAmount() {return player.oi}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "o", description: "O: Reset for ois", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true}
+})
